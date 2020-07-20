536faf1d9a6c94d897e6a53d9467c32f95aa6547
Neal <loseveny@qq.com>
2020-07-20 13:00:12 +1200

    update the mixed season report

--- src/js/report.js
+++ src/js/report.js
@@ -32,27 +32,48 @@ export default function(){
             delete reportData.response.data['barrick@moustacherepublic.com']
 
             let labelsArray = [];
-            let hoursArray = [];
+            let normalHoursArray = [];
+            let extraHoursArray = [];
             let pointsArray = [];
             
             $.each(reportData.response.data, (email, data)=>{
                 labelsArray.push(email.replace('@moustacherepublic.com', ''));
-                let totalHours = Number(data.totalExtraKPIHours) + Number(data.totalNormalKPIHours)
-                hoursArray.push(totalHours);
+                normalHoursArray.push(data.totalNormalKPIHours);
+                extraHoursArray.push(data.totalExtraKPIHours)
                 pointsArray.push(data.totalPoints);
             })
 
-            initChart(labelsArray, hoursArray, pointsArray);
+            initChart(labelsArray, normalHoursArray, extraHoursArray, pointsArray);
         }
 
-        function initChart(labelsArray, hoursArray, pointsArray){
+        function initChart(labelsArray, normalHoursArray, extraHoursArray, pointsArray){
             var myChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
                     labels: labelsArray,
                     datasets: [{
-                        label: 'KPI hours',
-                        data: hoursArray,
+                        label: 'Normal KPI hours',
+                        data: normalHoursArray,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    },{
+                        label: 'Extra KPI hours',
+                        data: extraHoursArray,
                         backgroundColor: [
                             'rgba(255, 99, 132, 0.2)',
                             'rgba(54, 162, 235, 0.2)',
