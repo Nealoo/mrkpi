fb2e3cdd92b19c80ebcf9235fe82ce1d0c3899d6
Neal <loseveny@qq.com>
2021-01-22 18:13:48 +1300

    add text to show the current week number

--- src/js/tl.js
+++ src/js/tl.js
@@ -8,6 +8,22 @@ import {baseUrl} from './utils/env';
 export default function(){
     report('tl');
 
+    function getWeekNumber(d) {
+        // Copy date so don't modify original
+        d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+        // Set to nearest Thursday: current date + 4 - current day number
+        // Make Sunday's day number 7
+        d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+        // Get first day of year
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+        // Calculate full weeks to nearest Thursday
+        var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+        // Return array of year and week number
+        return weekNo;//[d.getUTCFullYear(), weekNo];
+    }
+
+    document.querySelector('#kpi-current-week-number').innerHTML = getWeekNumber(new Date());
+
     function getBasicInfo(){
         var kpiEmail = document.querySelector("#kpi-form-email").value;
         var kpiKey = document.querySelector("#kpi-form-key").value;
